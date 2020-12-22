67b977217de363f7b514d2281f8dbf2cdf807f9e
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-12-21 21:06:56 -0300

    @Component, @Autowired, @SpringBootApplication

--- springboot2-essentials/src/main/java/com/everton/springboot2/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/com/everton/springboot2/controller/AnimeController.java
@@ -1,5 +1,6 @@
 package com.everton.springboot2.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.everton.springboot2.domain.Anime;
+import com.everton.springboot2.util.DateUtil;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
+
+	private final DateUtil dateUtil;
 
 	@GetMapping(path = "list")
 	public List<Anime> list() {
+		log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
 		return List.of(new Anime("DBZ"), new Anime("Berserk"));
 
 	}
