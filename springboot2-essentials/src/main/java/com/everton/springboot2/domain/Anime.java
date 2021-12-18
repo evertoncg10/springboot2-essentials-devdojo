4ccde296d9f59016c5db2885d3b05021b30aab91
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2021-12-18 14:04:58 -0300

    Gerando projeto com start.spring.io

--- springboot2-essentials/src/main/java/com/everton/springboot2/domain/Anime.java
+++ springboot2-essentials/src/main/java/com/everton/springboot2/domain/Anime.java
@@ -1,5 +1,10 @@
 package com.everton.springboot2.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Anime {
 	private String name;
 
@@ -8,14 +13,6 @@ public class Anime {
 
 	public Anime(String name) {
 		this.setName(name);
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 }
