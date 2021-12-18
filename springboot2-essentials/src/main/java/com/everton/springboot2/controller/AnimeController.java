902c66cc5adb3dde7004abf7e03f996ef0d049cf
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2021-12-18 14:54:47 -0300

    Aula 08 - Método GET parte 2.

--- springboot2-essentials/src/main/java/com/everton/springboot2/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/com/everton/springboot2/controller/AnimeController.java
@@ -4,7 +4,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.everton.springboot2.service.AnimeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +27,15 @@ public class AnimeController {
 	private final AnimeService animeService;
 
 	@GetMapping
-	public List<Anime> listAll() {
+	public  ResponseEntity<List<Anime>> listAll() {
 		log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
-		return animeService.listAll();
+		return ResponseEntity.ok(animeService.listAll());
+
+	}
+
+	@GetMapping(path = "/{id}")
+	public  ResponseEntity<Anime> findById(@PathVariable long id) {
+		return ResponseEntity.ok(animeService.findById(id));
 
 	}
 }
