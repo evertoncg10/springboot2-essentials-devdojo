2304418e97ae2de6486578d7c5ad286d013f8e2c
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2022-01-15 15:57:33 -0300

    Aula 23 - Paginação.

--- springboot2-essentials/src/main/java/com/everton/springboot2/service/AnimeService.java
+++ springboot2-essentials/src/main/java/com/everton/springboot2/service/AnimeService.java
@@ -7,10 +7,10 @@ import com.everton.springboot2.repository.AnimeRepository;
 import com.everton.springboot2.requests.AnimePostRequestBody;
 import com.everton.springboot2.requests.AnimePutRequestBody;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,8 +19,8 @@ import java.util.List;
 public class AnimeService {
      private final AnimeRepository animeRepository;
 
-    public List<Anime> listAll() {
-        return animeRepository.findAll();
+    public Page<Anime> listAll(Pageable pageable) {
+        return animeRepository.findAll(pageable);
     }
 
     public List<Anime> findByName(String name) {
