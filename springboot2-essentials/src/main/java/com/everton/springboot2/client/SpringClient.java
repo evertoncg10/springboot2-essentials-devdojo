e40c29d30c5f5a8008ab14249e62124bc7b439fd
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2022-01-15 19:47:18 -0300

    Aula 28 - RestTemplate POST.

--- springboot2-essentials/src/main/java/com/everton/springboot2/client/SpringClient.java
+++ springboot2-essentials/src/main/java/com/everton/springboot2/client/SpringClient.java
@@ -3,8 +3,7 @@ package com.everton.springboot2.client;
 import com.everton.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -28,5 +27,22 @@ public class SpringClient {
                 new ParameterizedTypeReference<>() {
                 });
         log.info(exchange.getBody());
+
+//        Anime kindom = Anime.builder().name("Kindom").build();
+//        Anime kindomSaved = new RestTemplate().postForObject("http://localhost:8080/animes/", kindom, Anime.class);
+//        log.info(kindomSaved);
+
+        Anime samuraiChamplog = Anime.builder().name("Samurai Champlog").build();
+        ResponseEntity<Anime> samuraiChamplogSaved = new RestTemplate().exchange("http://localhost:8080/animes/",
+                HttpMethod.POST,
+                new HttpEntity<>(samuraiChamplog, createJsonHeader()),
+                Anime.class);
+        log.info("Saved anime {}", samuraiChamplogSaved);
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
