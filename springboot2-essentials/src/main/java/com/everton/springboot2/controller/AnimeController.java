478a9925a8b2dd723f71ed6a9cd27a2f1ceecf2e
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2022-01-15 11:45:22 -0300

    Aula 20 - Validação de campos.

--- springboot2-essentials/src/main/java/com/everton/springboot2/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/com/everton/springboot2/controller/AnimeController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -43,7 +44,7 @@ public class AnimeController {
 	}
 
 	@PostMapping
-	public ResponseEntity<Anime> save(@RequestBody AnimePostRequestBody animePostRequestBody) {
+	public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
 		//TODO: Verificar a utilização do Response Entity de forma adequada para não gerar warning
 		return new ResponseEntity(animeService.save(animePostRequestBody), HttpStatus.CREATED);
 	}
