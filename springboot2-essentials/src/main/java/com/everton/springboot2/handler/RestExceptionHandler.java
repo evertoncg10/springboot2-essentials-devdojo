d3554d9cc2896313b1cc0a509545d5662306089a
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2022-01-15 11:21:46 -0300

    Aula 18 - Handler Global.

---
+++ springboot2-essentials/src/main/java/com/everton/springboot2/handler/RestExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.everton.springboot2.handler;
+
+import com.everton.springboot2.exception.BadRequestException;
+import com.everton.springboot2.exception.BadRequestExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException exception) {
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timestap(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Check the Documentation")
+                        .details(exception.getMessage())
+                        .developerMessage(exception.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+}
